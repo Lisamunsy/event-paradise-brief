1f53808e450eec5271bf550f43c78f7119fd24ae
Lisa Munsy <stillailleurs@gmail.com>
2022-12-10 13:59:10 +0100

    Deleted aria-describedby, tried to reorganize script.js file

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,64 @@ const toast = new bootstrap.Toast(toastElement, {
     delay: 5000
 });
 
+for (let i = 0; i < inputs.length; i++) {
+    const element = inputs[i];
+    const helpText = document.querySelector(`#${element.id}Help`);
+    let message ="";
+    let tooltip = null;
+
+    element.addEventListener('invalid', (e) => {
+        e.preventDefault();
+        helpText.classList.add("text-danger");
+        element.classList.add( "is-invalid");
+
+        message = selectTooltipMessage(element)
+        tooltip = createTooltip(element, message);
+        const firstInvalid = form.querySelector(':invalid');
+
+        if (element === firstInvalid) {
+            tooltip.show();
+            element.focus()
+        } 
+    });
+    
+    element.addEventListener('change', (e) => {
+        element.checkValidity();
+        if (element.validity) {
+            helpText.classList.remove("text-danger");
+            helpText.classList.add("text-success");
+            element.classList.remove("is-invalid");
+            element.classList.add("is-valid");
+            if (tooltip != null) {
+                const tooltips =document.querySelectorAll(".tooltip");
+                tooltips.forEach(element => {
+                    element.remove();
+                });
+                tooltip.dispose();
+            } 
+        }
+    })
+}
+
+function selectTooltipMessage(element ){
+    if (element.validity.valueMissing) {         
+        return "Champ obligatoire";
+    } else if (element.value < element.min && element.type === "number"){
+        return "Doit être positif";
+    } else if (element.type === "date") {
+        return "Doit être égale ou supérieure à aujourd'hui";
+    }
+}
+
+function createTooltip(element, message) {
+
+    return (new bootstrap.Tooltip(element, {
+        title : message,
+        placement : "bottom",
+        trigger: "manual"
+    }));
+}
+
 form.addEventListener('submit', event => {
     event.preventDefault();
     form.reset();
@@ -17,68 +75,4 @@ form.addEventListener('submit', event => {
         }
     }
     toast.show()
-    console.log("implement and toaster")  ;  
 })
-
-handleValidation();
-
-
-function handleValidation() {
-    for (let i = 0; i < inputs.length; i++) {
-        const element = inputs[i];
-        let message ="";
-        let tooltip = null;
-        element.addEventListener('invalid', (e) => {
-            e.preventDefault();
-
-            const helpText = document.querySelector(`#${e.target.id}Help`);
-            helpText.classList.add("text-danger");
-            e.target.classList.add( "is-invalid");
-    
-            
-            if (e.target.validity.valueMissing) {         
-                message ="Champ obligatoire";
-            } else if (e.target.value < e.target.min && e.target.type === "number"){
-                message ="Doit être positif";
-            } else if (e.target.type === "date") {
-                message ="Doit être égale ou supérieure à aujourd'hui";
-            }
-
-            tooltip = createTooltip(element, message);
-            let firstInvalid = form.querySelector(':invalid');
-            
-            console.log(firstInvalid);
-            if (element === firstInvalid) {
-               tooltip.show();
-               element.focus()
-            } 
-        });
-        
-        element.addEventListener('change', (e) => {
-            element.checkValidity();
-            if (element.validity) {
-                const helpText = document.querySelector(`#${element.id}Help`);
-                helpText.classList.remove("text-danger");
-                helpText.classList.add("text-success");
-                element.classList.remove("is-invalid");
-                element.classList.add("is-valid");
-                if (tooltip != null) {
-                    const tooltips =document.querySelectorAll(".tooltip");
-                    tooltips.forEach(element => {
-                        element.remove();
-                    });
-                    tooltip.dispose();
-                } 
-            }
-        })
-    }
-}
-
-function createTooltip(element, message) {
-
-    return (new bootstrap.Tooltip(element, {
-        title : message,
-        placement : "bottom",
-        trigger: "manual"
-    }));
-}
